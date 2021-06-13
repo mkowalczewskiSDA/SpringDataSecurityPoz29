f841ce187cea4d3982599dca77b1647af3774755
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-13 15:02:25 +0200

    Orders

--- src/main/java/com/example/SpringDataSecurity/controller/OrderController.java
+++ src/main/java/com/example/SpringDataSecurity/controller/OrderController.java
@@ -1,8 +1,10 @@
-package com.example.SpringDataSecurity;
+package com.example.SpringDataSecurity.controller;
 
 import com.example.SpringDataSecurity.repository.OrderRepository;
+import com.example.SpringDataSecurity.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrderController {
 
     @Autowired
-    OrderRepository orderRepository;
+    OrderService orderRepository;
 
     @RequestMapping("/list")
-    public String getOrders() {
-
-        return null;
+    public String getOrders(Model model) {
+        model.addAttribute("orders", orderRepository.getAllOrders());
+        return "index";
     }
 
 }
