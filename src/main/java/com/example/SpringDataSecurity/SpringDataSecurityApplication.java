8e2339702fb2140648ab1e852ea3b0b3c466733f
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-13 13:06:02 +0200

    first draft

---
+++ src/main/java/com/example/SpringDataSecurity/SpringDataSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataSecurity/SpringDataSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
