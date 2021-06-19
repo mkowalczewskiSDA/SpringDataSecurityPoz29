34a1b8c3ebdc9198baca27cb0f2b97ecf4a54ef0
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-19 14:42:57 +0200

    logout

--- src/main/java/com/example/SpringDataSecurity/controller/HomeController.java
+++ src/main/java/com/example/SpringDataSecurity/controller/HomeController.java
@@ -5,9 +5,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
 
 @Controller
@@ -26,6 +30,18 @@ public class HomeController {
     @GetMapping("/admin/page")
     public String adminPage() {
         return "admin";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage(HttpServletRequest request
+            , HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder
+                .getContext().getAuthentication();
+        if (authentication.isAuthenticated()) {
+            new SecurityContextLogoutHandler()
+                    .logout(request, response, authentication);
+        }
+        return "redirect:/login?logout";
     }
 
     @GetMapping("/user")
