2da0f6fcde53ab1301a99d9380631637efcc0dfe
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-19 12:02:23 +0200

    price sum

---
+++ src/main/java/com/example/SpringDataSecurity/configuration/WebSecurityConf.java
@@ -0,0 +1,15 @@
+package com.example.SpringDataSecurity.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+public class WebSecurityConf extends WebSecurityConfigurerAdapter {
+
+
+
+}
