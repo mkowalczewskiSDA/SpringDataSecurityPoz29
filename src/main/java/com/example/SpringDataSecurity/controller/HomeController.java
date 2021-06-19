8258151094d6f24c3ce9288fc69286805f9709e0
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-19 13:37:50 +0200

    auth page

--- src/main/java/com/example/SpringDataSecurity/controller/HomeController.java
+++ src/main/java/com/example/SpringDataSecurity/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.example.SpringDataSecurity.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class HomeController {
@@ -9,5 +16,24 @@ public class HomeController {
     @GetMapping("/home")
     public String homePage() {
         return "home";
+    }
+
+    @GetMapping("/admin/page")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    //@PostAuthorize("hasAuthority('ADMIN')")
+    //@PreAuthorize("hasAuthority('ADMIN')")
+    public String userPage(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        if(authorities.contains(new SimpleGrantedAuthority("ADMIN"))) {
+            return "userPage";
+        } else {
+            return "home";
+        }
+        /*System.out.println("ten kod działa");
+        return "userPage";*/
     }
 }
